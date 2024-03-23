1c7c5bea919e1707369ae4316e03067899a850fb
Alejandro Vialard Santana <a.vialard.developer@gmail.com>
2024-03-23 16:44:21 +0000

    FEAT: Cambio de idioma
    Se ha configurado la app para poder cambiar de español
    a inglés usando la propia internalización de Android Studio
    con Locale.

---
+++ app/src/main/java/com/avs/pantrychef/controller/IngredientController.kt
@@ -0,0 +1,9 @@
+package com.avs.pantrychef.controller
+
+import com.avs.pantrychef.model.Ingredient
+import com.google.firebase.firestore.FirebaseFirestore
+
+class IngredientController {
+
+    private val db = FirebaseFirestore.getInstance()
+}
--- app/src/main/java/com/avs/pantrychef/view/activities/HomeActivity.kt
+++ app/src/main/java/com/avs/pantrychef/view/activities/HomeActivity.kt
@@ -10,12 +10,18 @@ import androidx.navigation.ui.setupWithNavController
 import com.avs.pantrychef.R
 import com.avs.pantrychef.controller.AuthController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import java.util.Locale
+import android.content.Context
+import android.content.res.Configuration
+import android.os.Build
+
 class HomeActivity: AppCompatActivity() {
 
     private lateinit var authController: AuthController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_home)
 
         authController = AuthController(this)
@@ -30,23 +36,56 @@ class HomeActivity: AppCompatActivity() {
         setupUserIconMenu()
     }
 
+    override fun attachBaseContext(newBase: Context?) {
+        val sharedPref = newBase?.getSharedPreferences("PantryChef", Context.MODE_PRIVATE)
+        val languageCode = sharedPref?.getString("language", Locale.getDefault().language) ?: Locale.getDefault().language
+        applyLocale(newBase, languageCode)
+
+        super.attachBaseContext(newBase)
+    }
+
+    private fun applyLocale(context: Context?, languageCode: String) {
+        val locale = Locale(languageCode)
+        Locale.setDefault(locale)
+        val config = Configuration()
+        config.setLocale(locale)
+        context?.resources?.updateConfiguration(config, context.resources.displayMetrics)
+    }
+
     private fun setupUserIconMenu() {
         val userIcon = findViewById<ImageView>(R.id.userIcon)
         userIcon.setOnClickListener {
             val popupMenu = PopupMenu(this, userIcon)
             popupMenu.menuInflater.inflate(R.menu.user_menu, popupMenu.menu)
+
+            val languageMenuItem = popupMenu.menu.findItem(R.id.changeLanguage)
+
+            // Cambiar el texto del item de cambio de idioma dependiendo del idioma actual
+            if (Locale.getDefault().language.equals("es", ignoreCase = true)) {
+                languageMenuItem.title = "Cambiar a Inglés"
+            } else {
+                languageMenuItem.title = "Change to Spanish"
+            }
+
+
             popupMenu.setOnMenuItemClickListener { item ->
                 when (item.itemId) {
                     R.id.logout -> {
                         authController.logOut()
-
-                        // Navegar a la pantalla main
                         val intent = Intent(this, MainActivity::class.java).apply {
                             flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
                         }
                         startActivity(intent)
                         finish()
-
+                        true
+                    }
+                    R.id.changeLanguage -> {
+                        // Cambiar el idioma y reiniciar la actividad
+                        if (Locale.getDefault().language.equals("es", ignoreCase = true)) {
+                            setLocale("en")
+                        } else {
+                            setLocale("es")
+                        }
                         true
                     }
                     else -> false
@@ -55,4 +94,36 @@ class HomeActivity: AppCompatActivity() {
             popupMenu.show()
         }
     }
+
+    private fun setLocale(languageCode: String, shouldRecreate: Boolean = true) {
+        val locale = Locale(languageCode)
+        Locale.setDefault(locale)
+
+        val resources = baseContext.resources
+        val config = resources.configuration
+        config.setLocale(locale)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            baseContext.createConfigurationContext(config)
+        } else {
+            resources.updateConfiguration(config, resources.displayMetrics)
+        }
+
+        if (shouldRecreate) {
+            recreate()
+        }
+
+        // Guardar el idioma seleccionado en las preferencias compartidas
+        saveLanguageToPreferences(languageCode)
+    }
+
+    private fun saveLanguageToPreferences(languageCode: String) {
+        val sharedPref = getSharedPreferences("PantryChef", Context.MODE_PRIVATE)
+        with(sharedPref.edit()) {
+            putString("language", languageCode)
+            apply()
+        }
+    }
+
+
 }
