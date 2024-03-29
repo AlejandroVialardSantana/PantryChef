f16bb870da505c264946d63d9fa0f4050521500f
Alejandro Vialard Santana <a.vialard.developer@gmail.com>
2024-03-29 18:44:12 +0000

    FEAT: Añadir y quitar de favoritos
    Se ha hecho la funcionalidad de poder guardar una
    receta como favorita desde el fragmento de recetas.
    Falta poder visualizarlas desde el fragmento de favoritos.

--- app/src/main/java/com/avs/pantrychef/controller/UserController.kt
+++ app/src/main/java/com/avs/pantrychef/controller/UserController.kt
@@ -1,14 +1,54 @@
 package com.avs.pantrychef.controller
 
 import com.avs.pantrychef.model.User
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
 
 class UserController {
 
     private val db = FirebaseFirestore.getInstance()
+    private val auth = FirebaseAuth.getInstance()
 
     fun createUser(user: User, onSuccess: () -> Unit, onFailure: (String) -> Unit) {
         db.collection("users").document(user.userId).set(user)
+            .addOnSuccessListener {
+                onSuccess()
+            }
+            .addOnFailureListener {
+                onFailure(it.message.toString())
+            }
+    }
+
+    fun addRecipeToFavorites(recipeId: String, onSuccess: () -> Unit, onFailure: (String) -> Unit) {
+        val userId = auth.currentUser?.uid ?: return onFailure("User not logged in")
+
+        db.collection("users").document(userId).update("favoriteRecipes", FieldValue.arrayUnion(recipeId))
+            .addOnSuccessListener {
+                onSuccess()
+            }
+            .addOnFailureListener {
+                onFailure(it.message.toString())
+            }
+    }
+
+    fun getFavoriteRecipes(onSuccess: (List<String>) -> Unit, onFailure: (String) -> Unit) {
+        val userId = auth.currentUser?.uid ?: return onFailure("User not logged in")
+
+        db.collection("users").document(userId).get()
+            .addOnSuccessListener { document ->
+                val favoriteRecipes = document.get("favoriteRecipes") as? List<String> ?: emptyList()
+                onSuccess(favoriteRecipes)
+            }
+            .addOnFailureListener {
+                onFailure(it.message.toString())
+            }
+    }
+
+    fun removeRecipeFromFavorites(recipeId: String, onSuccess: () -> Unit, onFailure: (String) -> Unit) {
+        val userId = auth.currentUser?.uid ?: return onFailure("User not logged in")
+
+        db.collection("users").document(userId).update("favoriteRecipes", FieldValue.arrayRemove(recipeId))
             .addOnSuccessListener {
                 onSuccess()
             }
--- app/src/main/java/com/avs/pantrychef/view/fragments/RecipeListFragment.kt
+++ app/src/main/java/com/avs/pantrychef/view/fragments/RecipeListFragment.kt
@@ -7,29 +7,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.navArgs
 import com.avs.pantrychef.R
 import com.avs.pantrychef.controller.RecipeController
+import com.avs.pantrychef.controller.UserController
 import com.avs.pantrychef.model.Recipe
 import java.util.Locale
 
 class RecipeListFragment : Fragment() {
 
     private val args: RecipeListFragmentArgs by navArgs()
+    private val userController = UserController()
+    private val favoriteRecipes = mutableListOf<String>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate el layout para este fragmento
-        return inflater.inflate(R.layout.fragment_receipt_list, container, false)
+        return inflater.inflate(R.layout.fragment_recipe_list, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        loadFavoriteRecipes()
 
         val ingredientIds = args.ingredientIds
 
@@ -54,13 +60,21 @@ class RecipeListFragment : Fragment() {
     }
 
     private fun addRecipeCardToView(recipe: Recipe, ingredientIds: Array<String>, container: LinearLayout) {
-        // Crear una vista de tarjeta para la receta
+        // Crear una vista de tarjeta para la recetas
         val cardView = layoutInflater.inflate(R.layout.card_recipe, container, false)
 
         // Encuentra los componentes de la vista
         val recipeName: TextView = cardView.findViewById(R.id.recipeName)
         val recipeTime: TextView = cardView.findViewById(R.id.recipeTime)
         val recipeDifficulty: TextView = cardView.findViewById(R.id.recipeDifficulty)
+        val favoriteIcon: ImageView = cardView.findViewById(R.id.favoriteIcon)
+
+        val isFavorite = recipe.id in favoriteRecipes
+        if (isFavorite) {
+            favoriteIcon.setImageResource(R.drawable.ic_fav_filled)
+        } else {
+            favoriteIcon.setImageResource(R.drawable.ic_favs)
+        }
 
         // Configurar los valores de los componentes de la vista
         recipeName.text = recipe.title
@@ -86,5 +100,37 @@ class RecipeListFragment : Fragment() {
         }
 
         container.addView(cardView)
+
+        favoriteIcon.setOnClickListener { view ->
+            val isFavorite = recipe.id in favoriteRecipes
+            if (isFavorite) {
+                userController.removeRecipeFromFavorites(recipe.id,
+                    onSuccess = {
+                        favoriteIcon.setImageResource(R.drawable.ic_favs) // Ícono para no favorito
+                        favoriteRecipes.remove(recipe.id)
+                    },
+                    onFailure = { error ->
+                        Log.e("RecipeListFragment", "Error removing recipe from favorites: $error")
+                    })
+            } else {
+                userController.addRecipeToFavorites(recipe.id,
+                    onSuccess = {
+                        favoriteIcon.setImageResource(R.drawable.ic_fav_filled) // Ícono para favorito
+                        favoriteRecipes.add(recipe.id)
+                    },
+                    onFailure = { error ->
+                        Log.e("RecipeListFragment", "Error adding recipe to favorites: $error")
+                    })
+            }
+        }
+    }
+
+    private fun loadFavoriteRecipes() {
+        userController.getFavoriteRecipes(onSuccess = { recipes ->
+            favoriteRecipes.clear()
+            favoriteRecipes.addAll(recipes)
+        }, onFailure = {
+            Log.e("RecipeListFragment", "Error loading favorite recipes: $it")
+        })
     }
 }
